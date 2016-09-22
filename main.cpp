f71aa8fed0549c4e8a43e682dee4182f5ac72e57
rosedge <rosedge@qq.com>
2016-09-22 21:32:25 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  OpenGL_1
+//
+//  Created by rosedge on 16/8/18.
+//  Copyright (c) 2016年 rosedge. All rights reserved.
+//
+
+#include <iostream>
+#include <GLUT/GLUT.h>
+#include <cstdio>
+
+
+void run() {
+    printf("hello\n");
+}
+
+int f() {
+    return 3[new int];
+}
+
+int main(int argc, char* argv[]) {
+    if (f() != f())
+        run();
+    return 0;
+}
